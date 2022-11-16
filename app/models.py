9ff922f0b27a9779f9ef2fb374ccc53d2ee40185
zalmek <zalmek23@gmail.com>
2022-11-16 23:01:14 +0300

    Second commit(Homework 2)

--- app/models.py
+++ app/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+QUESTIONS = [
+    {
+        'id': question_id,
+        'title': f'Question #{question_id}',
+        'text': f'Text of question #{question_id}',
+    } for question_id in range(10)
+]
